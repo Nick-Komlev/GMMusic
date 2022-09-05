1241cdbe39905f72e24fd58f39449fa42dcf5ace
Nikita <nikoder001@gmail.com>
2022-09-05 10:31:22 +0300

    Осуществлена привязка заголовка окна к свойству класса MainWindowViewModel

--- GMMusic/ViewModels/MainWindowViewModel.cs
+++ GMMusic/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GMMusic.ViewModels.Base;
 
 namespace GMMusic.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Свойство заголовока окна
+
+        private string _Title = "Медиаплеер GMMusic";
+
+        /// <summary>Заголовок окна</summary>
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
